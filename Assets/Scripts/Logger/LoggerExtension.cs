e3c2b2b3bc94a47cdce75a5d44e10ee3b6783e23
Ivan Kramarenko <ivan.kramarenko@whitesharx.com>
2023-08-23 03:41:50 +0300

    init

---
+++ Assets/Scripts/Logger/LoggerExtension.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Logger {
+  public static class LoggerExtension {
+    public static T Log<T>(this T obj) {
+      Debug.Log(obj);
+
+      return obj;
+    }
+
+    public static T Log<T>(this T obj, object prompt) {
+      Debug.Log(prompt + obj);
+
+      return obj;
+    }
+  }
+}
